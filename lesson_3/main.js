dbfb19bfe8b54d066321fcb6d47b97550de427ab
Jarvis <extasyvs@gmail.com>
2019-02-21 21:01:51 +0200

    script was comented

---
+++ lesson_3/main.js
@@ -0,0 +1,55 @@
+// First task
+          // let number= prompt('Enter a number');
+          // if(number>0){
+          //   alert('1')
+          // } else if(number<0){
+          //   alert('-1')
+          // } else if(number==0){
+          //   alert('0')
+          // }
+
+//Second Task using  "If-Else" construction!
+
+            // let login ='admin';
+            // let password='TheMaster';
+            // let accses=prompt("who's there ? ");
+            // if(accses===login){
+            //   let accsesSec=prompt('Enter your password,please: ');
+            //   if(accsesSec===password){
+            //     alert('Welcome')
+            //   }else if(accsesSec=='cancel'){
+            //     alert('Canceled.')
+            //   }else{
+            //     alert('Wrong password')
+            //   }
+            // } else if (accses=='cancel'){
+            //   alert('Canceled.')
+            // } else{
+            //   alert("I don't know you")
+            // }
+
+
+//Second Task using "Switch" construction
+    //   let login=prompt('enter your user');
+    //     switch (login){
+    //       case "admin":
+    //        let password=prompt('enter the password:');
+    //         switch(password){
+    //           case "TheMaster":
+    //           alert('Welcome');
+    //           break;
+    //           case "cancel":
+    //           alert('Canceled.');
+    //           break;
+    //           default:
+    //           alert('Wrong password');
+    //           break;
+    //         }
+    //     break;
+    //       case "cancel":
+    //       alert('Canceled.');
+    //       break;
+    //       default:
+    //       alert("I don't know you");
+    //       break;
+    //     }
